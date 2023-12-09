c010491b2e06af6d38a0a178e81ed72dbbc0d543
Forester302 <4rester302@gmail.com>
2023-12-09 11:30:05 +0000

    update sql.php

--- sql.php
+++ sql.php
@@ -22,6 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Query executed successfully";
     } else {
         echo "Error executing query: " . $conn->error;
+        echo $result;
     }
 }
 
